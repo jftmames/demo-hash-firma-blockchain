d86a496970de99b4fa6481f334053530467dfd20
Jose Fernández Tamame <jftamames@gmail.com>
2025-09-18 01:20:27 +0200

    Update page.tsx

--- app/labs/firma/page.tsx
+++ app/labs/firma/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 'use client';
 import { useEffect, useState, useCallback } from 'react';
 import { genECDSA, signECDSA, verifyECDSA, exportJWK, importJWK } from '@/lib/crypto';
